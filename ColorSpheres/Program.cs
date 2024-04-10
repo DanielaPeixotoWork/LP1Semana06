1945148fa41c7fc0d34ee2291a3c1ca27ed16cfb
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-04-10 12:28:36 +0100

    Adição da classe program

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -39,6 +39,49 @@ namespace ColorSpheres
             Radius = radius;
             TimesThrown = 0;
         }
+
+        public void Pop()
+        {
+            Radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (Radius > 0)
+            {
+                TimesThrown++;
+            }
+        }
+
+        public int GetTimesThrown()
+        {
+            return TimesThrown;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sphere sphere1 = new Sphere(new Color(255, 0, 0), 5.0);
+            Sphere sphere2 = new Sphere(new Color(0, 255, 0), 7.0);
+            Sphere sphere3 = new Sphere(new Color(0, 0, 255), 6.0);
+
+            sphere1.Throw();
+            sphere2.Throw();
+            sphere3.Throw();
+
+            sphere1.Pop();
+
+            Console.WriteLine("Sphere 1: Color (R: {0}, G: {1}, B: {2}), Radius: {3}, Times Thrown: {4}",
+                              sphere1.Color.Red, sphere1.Color.Green, sphere1.Color.Blue,
+                              sphere1.Radius, sphere1.GetTimesThrown());
+            Console.WriteLine("Sphere 2: Color (R: {0}, G: {1}, B: {2}), Radius: {3}, Times Thrown: {4}",
+                              sphere2.Color.Red, sphere2.Color.Green, sphere2.Color.Blue,
+                              sphere2.Radius, sphere2.GetTimesThrown());
+            Console.WriteLine("Sphere 3: Color (R: {0}, G: {1}, B: {2}), Radius: {3}, Times Thrown: {4}",
+                              sphere3.Color.Red, sphere3.Color.Green, sphere3.Color.Blue,
+                              sphere3.Radius, sphere3.GetTimesThrown());
+        }
     }
 }
-
